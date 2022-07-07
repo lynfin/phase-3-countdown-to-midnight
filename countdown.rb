89928ecf359fcb072405cef71a882ed0815f8bf6
Lyn <lynfinman@gmail.com>
2022-07-07 13:25:08 -0400

    commit tree 8238978fcfb45c3513a3a1612c58501fec44e881

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
+# write your code here
 
-def countdown
+def countdown(start)
+  while start > 0
+    puts "#{start} SECOND(S)!"
+    start -= 1
+  end
+  'HAPPY NEW YEAR!'
 end
+
+def countdown_with_sleep(start)
+  while start > 0
+    puts "#{start} SECOND(S)!"
+    start -= 1
+    sleep 1
+  end
+  'HAPPY NEW YEAR!'
+end
+# puts countdown(10)
+# puts countdown_with_sleep(10)
